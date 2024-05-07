35bc68dad3827c532682d8975abfbee677463d26
thecodingrunner <finn.moffett@gmail.com>
2024-05-07 16:05:57 +0100

    Set up structure for dashboard, with CRUD actions for adding, editing and deleting products

---
+++ components/ProductForm.js
@@ -0,0 +1,81 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+export default function ProductForm({
+    _id,
+    title:existingTitle, 
+    description:existingDescription, 
+    price:existingPrice,
+    images,
+}) {
+    const [title, setTitle] = useState(existingTitle || '')
+    const [description, setDescription] = useState(existingDescription || '')
+    const [price, setPrice] = useState(existingPrice || '')
+    const [goToProducts, setGoToProducts] = useState(false)
+    const router = useRouter()
+    async function saveProduct(e) {
+        e.preventDefault()
+        const data = {title,description,price}
+        if (_id) {
+            await axios.put('/api/products', {...data, _id})
+        } else {
+            await axios.post('/api/products', data)
+        }
+        setGoToProducts(true)
+    }
+    if (goToProducts) {
+        router.push('/products')
+    }
+    function uploadImages(e) {
+        const files = e.target?.files;
+        if (files.length > 0) {
+            
+        }
+    }
+    return (
+            <form onSubmit={saveProduct}>
+                <label>Product name</label>
+                <input 
+                    type="text" 
+                    placeholder="product name" 
+                    value={title} 
+                    onChange={(e) => setTitle(e.target.value)} 
+                />
+                <label>
+                    Photos
+                </label>
+                <div className="mb-2">
+                    <label className="w-24 h-24 cursor-pointer flex flex-col items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+                        </svg>
+                        <div>
+                            Upload
+                        </div>
+                        <input type="file" onChange={uploadImages} className="hidden"/>
+                    </label>
+                    {!images?.length && (
+                        <div>No photos in this product</div>
+                    )}
+                </div>
+                <label>Description</label>
+                <textarea 
+                    placeholder="description" 
+                    value={description} 
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+                <label>Price (in GBP)</label>
+                <input 
+                    type="number" 
+                    placeholder="price"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)} 
+                />
+                <button 
+                    type="submit" 
+                    className="btn-primary"
+                >Save</button>
+            </form>
+    )
+}
