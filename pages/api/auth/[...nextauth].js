83dbca82ec6eb3342295fe57b8f1262c8d060a87
thecodingrunner <finn.moffett@gmail.com>
2024-05-08 12:45:45 +0100

    Restrict authorised gmail accounts

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,10 @@
-import NextAuth from "next-auth"
+import NextAuth, { getServerSession } from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import clientPromise from "../../../lib/db"
+
+const adminEmails = ['finn.moffett@googlemail.com']
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -11,5 +14,23 @@ export const authOptions = {
     })
   ],
   adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    session: ({session,token,user}) => {
+      if (adminEmails.includes(session?.user?.email)) {
+        return session
+      } else {
+        return false
+      }
+    },
+  }
 }
 export default NextAuth(authOptions)
+
+export async function isAdminRequest(req,res) {
+  const session = await getServerSession(req,res,authOptions);
+  if (!adminEmails.includes(session?.user?.email)) {
+    res.status(401)
+    res.end()
+    throw 'not an admin'
+  }
+}
